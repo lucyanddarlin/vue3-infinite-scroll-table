efab775b28ef7fec941851f90a665e24985d84c5
lucy.li <lucy.li@quectel.com>
2024-05-14 11:42:49 +0800

    feat: 临时调整 updateStoreItem 逻辑

--- src/ScrollTable/useStore.ts
+++ src/ScrollTable/useStore.ts
@@ -19,11 +19,13 @@ export default function useStore(): UseStoreReturn {
   }
 
   const updateStoreItem = (newItem: StoreItem) => {
+    console.log('updateStoreItem', newItem)
     const index = store.value.findIndex((item) => item.id === newItem.id)
     if (index !== -1) {
       store.value.splice(index, 1, newItem)
     } else {
-      store.value.push(newItem)
+      // TODO: 调整逻辑
+      store.value.unshift(newItem)
     }
   }
 
